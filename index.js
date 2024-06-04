60bd76519373f31cb92fc82830562fd79f44372c
Shahin-Mahmud98 <smshahinmahmud6@gmail.com>
2024-06-04 16:18:52 +0600

    added server-file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = 5000
